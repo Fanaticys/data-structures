ef18b7150bb1b063a7b4d1bb023afb3d67ec0ff5
Fanaticys <dmkndrtnk@gmail.com>
2022-09-04 18:58:39 +0300

    Add Tree, Graph

---
+++ src/TreeMaxHeap.js
@@ -0,0 +1,70 @@
+class TreeMaxHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  add(element) {
+    this.values.push(element);
+    let index = this.values.length - 1;
+    const current = this.values[index];
+
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+
+      if (parent <= current) {
+        this.values[parentIndex] = current;
+        this.values[index] = parent;
+        index = parentIndex;
+      } else break;
+    }
+  }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+
+    let index = 0;
+    const length = this.values.length;
+    const current = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > current) swap = leftChildIndex;
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild > current) ||
+          (swap !== null && rightChild > leftChild)
+        )
+          swap = rightChildIndex;
+      }
+
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = current;
+      index = swap;
+    }
+
+    return max;
+  }
+}
+
+const tree = new TreeMaxHeap();
+tree.add(10);
+tree.add(45);
+tree.add(38);
+tree.add(23);
+tree.add(32);
+tree.add(57);
+tree.add(36);
+console.log(tree.values);
+console.log(tree.extractMax());
+console.log(tree.values);
